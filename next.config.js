78c635a1fc9f8355066dc8c527ac1bbfbce13522
Ben <55542035+BritishFalcon@users.noreply.github.com>
2025-06-06 12:39:00 +0100

    Fix BACKEND_URL config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const backendUrl = process.env.BACKEND_URL || (process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : '');
+const backendUrl =
+  process.env.BACKEND_URL ||
+  (process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000" : "");
 
 const nextConfig = {
   rewrites: async () => {
